d06203520c87973f4d022d04f136b770899d978e
Whitney Waldinger <118254799+whitneywaldinger@users.noreply.github.com>
2023-12-09 20:36:19 -0800

    Add files via upload

--- FinalProject.py
+++ FinalProject.py
@@ -1,4 +1,11 @@
 import random
+k = 16
+
+band = lambda str1, str2: bin((int(str1, 2) & int(str2, 2)) % (2**k))[2:].zfill(k)
+bor = lambda str1, str2: bin((int(str1, 2) | int(str2, 2)) % (2**k))[2:].zfill(k)
+bxor = lambda str1, str2: bin((int(str1, 2) ^ int(str2, 2)) % (2**k))[2:].zfill(k)
+badd = lambda str1, str2: bin((int(str1, 2) + int(str2, 2)) % (2**k))[2:].zfill(k)
+bneg = lambda str1: ''.join('1' if bit == '0' else '0' for bit in str1)
 
 # this function takes an in integer value k, and returns a random string of 1s and 0s of length k
 def generateRandomString(k):
@@ -7,14 +14,6 @@ def generateRandomString(k):
             randnum = random.randint(0,100)
             strr += str(randnum % 2)
         return strr
-
-band = lambda str1, str2: bin((int(str1, 2) & int(str2, 2)) % (2 ** k))[2:].zfill(k)
-bor = lambda str1, str2: bin((int(str1, 2) | int(str2, 2)) % (2 ** k))[2:].zfill(k)
-bxor = lambda str1, str2: bin((int(str1, 2) ^ int(str2, 2)) % (2 ** k))[2:].zfill(k)
-badd = lambda str1, str2: bin((int(str1, 2) + int(str2, 2)) % (2 ** k))[2:].zfill(k)
-bsub = lambda str1, str2: bin((int(str1, 2) - int(str2, 2)) % (2 ** k))[2:].zfill(k)
-
-
 
 class MMAPoracle:
     def __init__(self, k, IDP, ID, K1, K2, K3, K4):
@@ -26,79 +25,48 @@ class MMAPoracle:
         self.K2 = K2   # Shared key K2
         self.K3 = K3   # Shared key K3
         self.K4 = K4   # Shared key K4
-        print("Created MMAP protocol")
-        print()
-
             
-    def protocolRun(self):  
-        print("Running protocol")   
-        print()   
+    def protocolRun(self):    
         # Step 1: The reader sends a Hello message to the tag, which powers the tag.
-        print("Step 1: The reader sends a Hello message to the tag, which powers the tag.")
-        print()
+
         # Step 2: The tag responds with IDP.
-        print("Step 2: The tag responds with IDP.")
-        print()
 
         # Step 3: Based on the value of IDP, the reader looks up the values of K1, K2, K3, and K4.
         # The reader then generates two random bit strings n1 and n2 and sends a message to the tag consisting of three bit
         # strings, A = IDP ⊕ K1 ⊕ n1, B = (IDP ∧ K2) ∨ n1, and C = IDP + K3 + n2
-        print("Step #3")
         n1 = generateRandomString(self.k)
         n2 = generateRandomString(self.k)
-        print("n1 =", n1)
-        print("n2 =", n2)
-        A = bxor(self.IDP, bxor(self.K1, n1))
+
+        A = bxor(bxor(self.IDP, self.K1), n1)
         B = bor(band(self.IDP, self.K2), n1)
         C = badd(badd(self.IDP, self.K3), n2)
-        print("A =", A)
-        print("B =", B)
-        print("C =", C)
-        print()
+
         # Messages A and C are used to deliver the random numbers n1 and n2 to the tag without the adversary determining them. 
         # Messages B and D are used to authenticate the reader and tag, respectively. 
         # Message E is used to transmit the tag’s secret information, ID, to the reader.
 
         # Step 4: Upon receiving A, B, and C, the tag computes n1 = A ⊕ IDP ⊕ K1 and n2 = C − IDP − K3.
-        print("Step #4")
         new_n1 = bxor(bxor(A, self.IDP), self.K1)
-        new_n2 = bsub(bsub(C, self.IDP), self.K3)
-        print("Caclulated n1 =", new_n1)
-        print("Caclulated n2 =", new_n2)
-        print()
+        new_n2 = badd(badd(C, bneg(self.IDP)), bneg(self.K3))
+
         # The tag then checks if B = (IDP ∧ K2) ∨ n1. 
-        if B == bor(band(self.IDP,self.K2), n1):
-            print("B statement is true")
-            print()
+        if B == bor(band(self.IDP,self.K2), new_n1):
+
             # The tag authenticates the reader and proceeds to the next step. 
             # Step 5: The tag sends a message to the reader consisting of the bit strings D = (IDP ∨ K4) ∧ n2 and E = (ID + IDP) ⊕ n1.
-            print("Step 5")
-            D = bor(band(self.IDP, self.K4), n2)
-            E = bxor(badd(self.ID, self.IDP), n1)
-            print("D =", D)
-            print("E =", E)
-            print()
+            D = band(bor(self.IDP, self.K4), new_n2)
+            E = bxor(badd(self.ID, self.IDP), new_n1)
+
 
             # Step 6: The reader computes ID = E ⊕ n1 − IDP.
-            computed_ID = bsub(bxor(E, n1),self.IDP)
-            print("Step #6")
-            print("computed ID =", computed_ID)
-            print()
+            computed_ID = badd(bxor(E, n1),bneg(self.IDP))
 
             #Step 7: The tag and reader each update the values of IDP, K1, K2, K3, and K4.
-            print("Step #7")
             self.IDP = bxor(badd(self.IDP, bxor(n1, n2)), self.ID)
             self.K1 = bxor(bxor(self.K1, n2), badd(self.K3, self.ID))
             self.K2 = bxor(bxor(self.K2, n2), badd(self.K4, self.ID))
             self.K3 = badd(bxor(self.K3, n1), bxor(self.K1, self.ID))
             self.K4 = badd(bxor(self.K4, n1), bxor(self.K2, self.ID))
-
-            print("IDP =", self.IDP)
-            print("K1  =", self.K1)
-            print("K2  =", self.K2)
-            print("K3  =", self.K3)
-            print("K4  =", self.K4)
-            print()
 
         else:
             # Otherwise the tag terminates the protocol.
@@ -117,55 +85,68 @@ class MMAPoracle:
         # Return 1 if correct, 0 otherwise
         return self.ID == given_ID
 
-# Example values for ID, IDP, K1, K2, K3, K4, and k (length of bit strings)
-ID = "1101"  # Example 4-bit ID
-IDP = "0011" # Example 4-bit IDP
-K1 = "0101"  # Example 4-bit key K1
-K2 = "1010"  # Example 4-bit key K2
-K3 = "1100"  # Example 4-bit key K3
-K4 = "1001"  # Example 4-bit key K4
-k = 4        # Length of the bit strings
+# MMAP Attack 
 
-# Create an instance of MMAPoracle
-mmap_oracle = MMAPoracle(k, IDP, ID, K1, K2, K3, K4)
+# Eve has access to B and IDP
+# any bits of n1 corresponding to 0 bits of IDP are known to Eve
+# ID = (E XOR n1) - IDP 
+# runs until it knows all bits of ID
+def MMAP_Attack(oracle):
+    # Eve already knows IDP because it is a non-secret pseudonym
+    outStruct, __ = oracle.protocolRun()
+    # EVE has access to B = (IDP and  K2) or n1
+    # and IDP 
 
-# Run the protocol
-protocol_output = mmap_oracle.protocolRun()
-
-# Check if protocol_output is None (which indicates an error in authentication)
-if protocol_output:
-    print("Protocol Output:", protocol_output)
-    print()
-
-    # Verify the ID
-    is_id_correct = mmap_oracle.verifyID(ID)
-    print("Is the ID correct?", is_id_correct)
-    print()
-else:
-    print("Authentication failed during the protocol run.") 
-
-
-def MMAP_attack(oracle):
-    """
-    MMAP Attack to recover the secret ID using observations on B and IDP.
-    """
-    # Step 1: Eavesdrop on the protocol and observe B and IDP
-    outStruct, _ = oracle.protocolRun()
-    B = outStruct["B"]
     IDP = oracle.IDP
+    
+    B = outStruct.get('B')
 
-    # Step 2: Recover bits of n1 based on B and IDP
-    n1_recovered = "".join(['0' if IDP[i] == '0' else B[i] for i in range(len(B))])
+    N1 = ""
+    for i in range(oracle.k):
+        if IDP[i] == '0':
+            N1 += B[i]
+        else:
+            N1 += 'X'
 
-    # Step 3: Use the recovered n1 to determine ID based on E
-    E = outStruct["E"]
-    ID_recovered = bin(int(E, 2) ^ int(n1_recovered, 2) - int(IDP, 2))[2:].zfill(len(IDP))
+    # if IDP[i] = 0 then B[i] = N1[i] 
+    
+    # EVE also knows E
+    E = outStruct.get('E')
 
-    return ID_recovered
+    E_XOR_n1 = ""
+    for i in range(k):
+        if N1[i] =='X':
+            E_XOR_n1 += 'X'
+        elif N1[i] == E[i]:
+            E_XOR_n1 += '0'
+        else: #N1[i] != E[i]
+            E_XOR_n1 += '1'
+    
+    # make IDP negative
+    NIDP = bneg(IDP)
+    print("CHECKING NEAGTIVE FOR IDP")
+    print("IDP  =", IDP)
+    print("NIDP =", NIDP)
+    
+    ID = ""
+    carry = 0
+    for i in range(k):
+        if E_XOR_n1[i] == 'X':
+            ID += 'X'
+            carry = 0
+        else:
+            curr_add = (int(E_XOR_n1[i]) + int(NIDP[i]) + carry)
+            if curr_add > 1:
+                carry = 1
+            else:
+                carry = 0
+            ID += str(curr_add % 2)
 
-# Example of using the MMAP_attack function
-def run_MMAP_attack():
-    k = 16  # Set the desired length of bit strings
+    return ID
+
+
+def runMMAPAttack():
+    k = 16
     IDP = generateRandomString(k)
     ID = generateRandomString(k)
     K1 = generateRandomString(k)
@@ -173,21 +154,35 @@ def run_MMAP_attack():
     K3 = generateRandomString(k)
     K4 = generateRandomString(k)
 
-    # Create an instance of MMAPoracle
+    EVE_ID = 'X' * k
+
     oracle = MMAPoracle(k, IDP, ID, K1, K2, K3, K4)
+    print("REAL ID =", oracle.ID)
+    print()
+    i = 1
+    while 'X' in EVE_ID:
+        print("Iteration", i)
+        print("EVE ID =", EVE_ID)
+        print()
+        i = i + 1
 
-    print("Original ID:", ID)
+        curr_ID = MMAP_Attack(oracle)
+        print()
+        print("REAL ID =", oracle.ID)
+        print("CURR ID =", curr_ID)
 
-    # Run the MMAP_attack to recover the secret ID
-    recovered_ID = MMAP_attack(oracle)
+        newID = ""
+        for i in range(oracle.k):
+            if EVE_ID[i] == 'X':
+                newID += curr_ID[i]
+            else:
+                newID += EVE_ID[i]
 
-    print("Recovered ID:", recovered_ID)
+        EVE_ID = newID
+        print()
+    print("ID = ", EVE_ID)
+    print()
+    print("Verfication is ", oracle.verifyID(EVE_ID))
+    
 
-    # Verify the correctness of the recovered ID
-    if oracle.verifyID(recovered_ID):
-        print("Verification: Successful")
-    else:
-        print("Verification: Failed")
-
-# Run the example
-run_MMAP_attack()
+runMMAPAttack()
